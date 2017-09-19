64513b06730048fca5e5121cc5be218761e9bfaf
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 21:02:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var katzDeli = [];
+var katzDeli = ["ADA"];
 function takeANumber(katzDeliLine,customer_name){
   katzDeliLine.push(customer_name);
   return "Welcome, "+katzDeli[0]+". You are number "+(katzDeliLine[0].index + 1)+" in line";
