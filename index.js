59857e8b69c2ebc2a7a502dcf832d62617b24f3d
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 21:22:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,11 +14,12 @@ function nowServing(katzDeliLine){
   }
 }
 function currentLine(katzDeliLine){
+  var list = [];
   if (katzDeliLine.length == 0) {
     return "The line is currently empty.";
   } else{
     for (var i=0;i<katzDeliLine.length;i++){
-      var list = [];
+
       list.push(i +". "+katzDeliLine[i]+", ");
     }
     return "The line is currently: " + list;
