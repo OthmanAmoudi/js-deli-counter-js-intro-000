857737ce072cded896d6ea2bbcf7f1d1aa280db0
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 20:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var katzDeli = [];
 function takeANumber(katzDeliLine,customer_name){
   katzDeliLine.push(customer_name);
+  return "Welcome, "+katzDeli[0]+". You are number 1 in line";
 }
 
 function nowServing(katzDeliLine){
