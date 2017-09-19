8f727196c76607070b0b0b9a98c8c4cc637f86ec
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 20:55:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function takeANumber(katzDeliLine,customer_name){
 }
 
 function nowServing(katzDeliLine){
-  if katzDeliLine.length == 0 {
+  if (katzDeliLine.length == 0) {
     return "There is nobody waiting to be served!";
   } else{
     return katzDeliLine[0];
