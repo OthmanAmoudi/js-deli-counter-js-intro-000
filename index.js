3c82bc2e3e773ee73fe9786498b4ce20926e47dd
Othman AMD <red.fantastic@hotmail.com>
2017-09-19 20:52:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ function takeANumber(katzDeliLine,customer_name){
 
 function nowServing(katzDeliLine){
   if katzDeliLine.length == 0 {
-    return "Theres no one to be served";
+    return "There is nobody waiting to be served!";
   } else{
     return katzDeliLine[0];
     katzDeliLine.shift();
+  }
+}
+function currentLine(katzDeliLine){
+  if katzDeliLine.length == 0 {
+    return "The line is currently empty.";
+  } else{
+    for (i=0;i<katzDeliLine.length;i++){
+      var list = i +". "+katzDeliLine[i]+"\n";
+    }
+    return "The line is currently " + list;
   }
 }
